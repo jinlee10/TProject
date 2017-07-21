dc97b3ede8a7bcd4df9721f18f4f1462e31a6c4c
emanonastr <emanonastr@gmail.com>
2017-07-21 11:39:51 +0900

    Yolo.java

---
+++ src/com/tacademy/data/Yolo.java
@@ -0,0 +1,24 @@
+package com.tacademy.data;
+
+public class Yolo {
+	
+	private int yolo;
+	
+	public Yolo(){
+		
+	}
+
+	public int getYolo() {
+		return yolo;
+	}
+
+	public void setYolo(int yolo) {
+		this.yolo = yolo;
+	}
+
+	@Override
+	public String toString() {
+		return "Yolo [yolo=" + yolo + "]";
+	}
+	
+}
