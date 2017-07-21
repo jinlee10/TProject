d9265d79a8e57921cc90bdcccc303885ef522c37
emanonastr <emanonastr@gmail.com>
2017-07-21 09:24:40 +0900

    YOLO?

--- src/com/tacademy/data/Data.java
+++ src/com/tacademy/data/Data.java
@@ -4,4 +4,24 @@ public class Data {
 	// git prac
 	
 	// wanna control my version? go git
+	
+	private String name;
+	
+	public Data(){
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Data [name=" + name + "]";
+	}
+	
 }
