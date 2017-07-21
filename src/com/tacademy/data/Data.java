5fc0c8cfafce6584cee34edfc3292a82059d8e3b
emanonastr <emanonastr@gmail.com>
2017-07-21 09:32:32 +0900

    뭐 왜 뭐

--- src/com/tacademy/data/Data.java
+++ src/com/tacademy/data/Data.java
@@ -6,6 +6,7 @@ public class Data {
 	// wanna control my version? go git
 	
 	private String name;
+	private int age;
 	
 	public Data(){
 		
@@ -19,9 +20,17 @@ public class Data {
 		this.name = name;
 	}
 
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 	@Override
 	public String toString() {
-		return "Data [name=" + name + "]";
+		return "Data [name=" + name + ", age=" + age + "]";
 	}
 	
 }
