31ab24edc2b42eb9f90691108b586954fdaed59e
emanonastr <emanonastr@gmail.com>
2017-07-21 11:41:39 +0900

    yoloBranch1 커밋했어!

--- src/com/tacademy/data/Yolo.java
+++ src/com/tacademy/data/Yolo.java
@@ -3,6 +3,7 @@ package com.tacademy.data;
 public class Yolo {
 	
 	private int yolo;
+	private String yolo1;
 	
 	public Yolo(){
 		
@@ -15,10 +16,18 @@ public class Yolo {
 	public void setYolo(int yolo) {
 		this.yolo = yolo;
 	}
+	
+	public String getYolo1() {
+		return yolo1;
+	}
+
+	public void setYolo1(String yolo1) {
+		this.yolo1 = yolo1;
+	}
 
 	@Override
 	public String toString() {
-		return "Yolo [yolo=" + yolo + "]";
+		return "Yolo [yolo=" + yolo + ", yolo1=" + yolo1 + "]";
 	}
 	
 }
